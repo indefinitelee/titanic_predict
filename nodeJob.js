1223588f2aed154c98910b4064d11ceebdc0a49d
David Ashe <david.ashe@aexp.com>
2018-08-20 09:42:37 -0400

    got the json generated

--- nodeJob.js
+++ nodeJob.js
@@ -14,15 +14,41 @@ const getCsvAsJson = async (filePath) => {
 	// console.log(typeof data, data instanceof Buffer);
 	const rows = data.toString().split('\n');
 	// PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
-	const columns = rows[0].split(',').map(token => token.replace('\r', ''));
+	const columns = rows[0].split(',')
+		.map(token => token.replace('\r', ''))
+		.filter(str => str);
 	console.log(columns);
 
-	const dataRows = rows.slice(1).map((row, index) => {
-		return row.split(',').reduce((accumulator, field, index) => {
-			return accumulator[columns[index]] = field;
+	const dataRows = rows.slice(1);
+	const arrayOfObjects = dataRows.map((row, index) => {
+		let rowAsArray = row.split(',');
+		
+		if (rowAsArray.length === columns.length + 1) {
+			const before = rowAsArray.slice(0,3);
+			const lastAndFirst = rowAsArray.slice(3,5);
+			const fullName = (`${lastAndFirst[1]} ${lastAndFirst[0]}`).replace(/["]/g, '');
+			const after = rowAsArray.slice(5);
+			rowAsArray = before.concat([fullName]).concat(after);
+			console.log(rowAsArray);
+		}
+
+		return rowAsArray.reduce((accumulator, field, index) => {
+			const column = columns[index];
+			if (column) {
+				accumulator[column] = field;
+			}
+			return accumulator;
 		}, {});
 	});
-	console.log(dataRows[0]);
+	console.log('first object in array ->', arrayOfObjects[0]);
+	const filtered = arrayOfObjects.filter(obj => obj[columns[0]]);
+
+	fs.writeFile('titanic.json', JSON.stringify(filtered), (err) => {
+		if (err) throw err;
+		console.log('The file has been saved!');
+	});
 };
 
-getCsvAsJson('./train.csv');
+getCsvAsJson('./train.csv')
+	.then(() => console.log('done'))
+	.catch(e => console.log(e));
