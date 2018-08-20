849762a9292506846967e07fc4e33af7cc68662a
David Ashe <david.ashe@aexp.com>
2018-08-20 09:46:28 -0400

    cleaned code up

--- nodeJob.js
+++ nodeJob.js
@@ -1,23 +1,15 @@
 const fs = require('fs');
 const { promisify } = require('util');
 const readFile = promisify(fs.readFile);
-const writeFile = promisify(fs.writeFile);
 
 const getCsvAsJson = async (filePath) => {
-	let data;
-	try {
-		data = await readFile(filePath);
-	} catch (e) {
-		console.log('oh noes', e.name + ' ' + e.message);
-	}
+	const data = await readFile(filePath);
 
-	// console.log(typeof data, data instanceof Buffer);
 	const rows = data.toString().split('\n');
-	// PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
+	// EXPECTED COLUMNS: PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
 	const columns = rows[0].split(',')
 		.map(token => token.replace('\r', ''))
 		.filter(str => str);
-	console.log(columns);
 
 	const dataRows = rows.slice(1);
 	const arrayOfObjects = dataRows.map((row, index) => {
@@ -26,10 +18,9 @@ const getCsvAsJson = async (filePath) => {
 		if (rowAsArray.length === columns.length + 1) {
 			const before = rowAsArray.slice(0,3);
 			const lastAndFirst = rowAsArray.slice(3,5);
-			const fullName = (`${lastAndFirst[1]} ${lastAndFirst[0]}`).replace(/["]/g, '');
+			const fullName = lastAndFirst.reverse().join(' ').replace(/["]/g, '');
 			const after = rowAsArray.slice(5);
 			rowAsArray = before.concat([fullName]).concat(after);
-			console.log(rowAsArray);
 		}
 
 		return rowAsArray.reduce((accumulator, field, index) => {
@@ -40,15 +31,17 @@ const getCsvAsJson = async (filePath) => {
 			return accumulator;
 		}, {});
 	});
-	console.log('first object in array ->', arrayOfObjects[0]);
+
 	const filtered = arrayOfObjects.filter(obj => obj[columns[0]]);
 
 	fs.writeFile('titanic.json', JSON.stringify(filtered), (err) => {
-		if (err) throw err;
+		if (err) {
+			throw err;
+		}
 		console.log('The file has been saved!');
 	});
 };
 
 getCsvAsJson('./train.csv')
-	.then(() => console.log('done'))
-	.catch(e => console.log(e));
+.then(() => console.log('done'))
+.catch(e => console.log(e));
