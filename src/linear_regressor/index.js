2de6ed9b540b8896adc770c51ae0646022215ba2
David Ashe <david.ashe@aexp.com>
2018-08-25 10:46:30 -0400

    more

--- src/linear_regressor/index.js
+++ src/linear_regressor/index.js
@@ -1,43 +1,14 @@
-import tf from '@tensorflow/tfjs';
-
-// async function myFirstTfjs(features, labels) {
-//     const featureSize = features.length;
-//     const labelSize = labels.length
-
-//     if (featureSize !== labelSize) {
-//         Promise.reject(`features: ${featureSize}, labels: ${labelSize}. Not the same size!`);
-//     }
-
-//     // Create a simple model.
-//     const model = tf.sequential();
-//     model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-  
-//     // Prepare the model for training: Specify the loss and the optimizer.
-//     model.compile({
-//       loss: 'meanSquaredError',
-//       optimizer: 'sgd'
-//     });
-
-//     const sizePair = [features.length, 1];
-  
-//     // Generate some synthetic data for training. (y = 2x - 1)
-//     const xs = tf.tensor2d(features, sizePair);
-//     const ys = tf.tensor2d(labels, sizePair);
-  
-//     // Train the model using the data.
-//     await model.fit(xs, ys, {epochs: 250});
-  
-//     // Use the model to do inference on a data point the model hasn't seen.
-//     // Should print approximately 39.
-//     document.getElementById('micro_out_div').innerText +=
-//         model.predict(tf.tensor2d([20], [1, 1]));
-// }
+import * as tf from '@tensorflow/tfjs';
 
 class LinearRegressor {
 
     constructor() {
+        this.reset();
+    }
+
+    reset() {
         const model = tf.sequential();
-        const steup = { units: 1, inputShape: [1] }
+        const setup = { units: 1, inputShape: [1] }
         model.add(tf.layers.dense(setup));
       
         // Prepare the model for training: Specify the loss and the optimizer.
--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,11 @@
 import React, { Fragment, Component } from 'react';
-// import tf from '@tensorflow/tfjs';
+import LinearRegressor from '../linear_regressor';
+
+const styles = {
+    br: {
+        marginBottom: '20px'
+    }
+};
 
 class Main extends Component {
 
@@ -7,25 +13,40 @@ class Main extends Component {
         super(props);
 
         this.state = {
-            json: JSON.parse(props.json)
+            json: props.json
         };
 
         this.handleFeatureColumnChange = (eventIThink) => {
             console.log(eventIThink);
+            // this.lr.addFeaturesAndLabels();
         };
+
+        // this.handleLabelColumnChange = (eventIThink) => {
+        //     console.log(eventIThink);
+        // };
 
         this.handleLabelColumnChange = (eventIThink) => {
             console.log(eventIThink);
         };
 
-        this.handleLabelColumnChange = () => {
+        this.train = () => {
 
         };
+
+        this.predict = () => {
+
+        };
+
+        this.lr = new LinearRegressor();
+    }
+
+    componentDidMount() {
+        
     }
 
     render() {
         return (
-            <Fragment>
+            <div>
                 <header>
                     <h1>Titanic Predictor</h1>
                 </header>
@@ -44,6 +65,8 @@ class Main extends Component {
                         </select>
                     </section>
 
+                    {/* <div style={styles.br} />
+
                     <label>Select Label</label>
                     <section>
                         <select onChange={this.handleLabelColumnChange}>
@@ -55,15 +78,19 @@ class Main extends Component {
                                 </option>
                             ))}
                         </select>
-                    </section>
+                    </section> */}
 
-                    <button>Train</button>
+                    <div style={styles.br} />
+
+                    <button onClick={this.train}>Train</button>
+
+                    <div style={styles.br} />
 
                     <input type="text" onChange={this.setPredictValue}/>
-                    <button>Predict {this.target} from {}</button>
+                    <button onClick={this.predict}>Predict</button>
 
                 </main>
-            </Fragment>
+            </div>
         );
     }
 };
